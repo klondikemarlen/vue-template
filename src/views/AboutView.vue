524ec3dffe0a906eb8436beb0855f94d41c9f765
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-24 21:59:39 -0700

    :heavy_plus_sign: Add bootrap-vue.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,9 @@
 <template lang="pug">
 .about
   h1 This is an about page
+  div
+    b-button Button
+    b-button(variant="danger") Button
+    b-button(variant="success") Button
+    b-button(variant="outline-primary") Button
 </template>
