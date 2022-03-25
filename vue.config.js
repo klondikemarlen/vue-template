b15f1924eefb2be799a0be43d198a409095d0066
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-24 21:18:14 -0700

    :fire: Kill webpack customizations.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service")
 
-const webpackConfig = require("./config/webpack")
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: webpackConfig,
 })
